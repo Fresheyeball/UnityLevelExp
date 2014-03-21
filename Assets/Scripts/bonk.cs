51ae792b2770bb29ac58c0e38df50ef6796d77b8
Isaac Shapira <fresheyeball@gmail.com>
2014-03-20 21:05:20 -0600

    bonk

---
+++ Assets/Scripts/bonk.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bonk : MonoBehaviour {
+	
+	void OnCollisionEnter(Collision collision){
+		if (collision.relativeVelocity.magnitude > 2) {
+			audio.Play();
+		}
+	}
+}
